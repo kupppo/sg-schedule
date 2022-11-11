f822267e9f91965ca19f075a87114fa5b463326c
kupppo <kupppo@protonmail.com>
2022-11-11 22:38:24 +0000

    initial commit

---
+++ lib/scraper.ts
@@ -0,0 +1,83 @@
+import * as Cheerio from 'cheerio'
+import { parse as parseDateTime } from 'date-fns'
+import { zonedTimeToUtc } from 'date-fns-tz'
+
+function cleanText(input: string) {
+  return input.replace(/(\n|\t)+/g, '').trim()
+}
+
+function parseCellContents (
+  contents: Cheerio.Element,
+  type: string
+): string | string[] | Date | TwitchChannel | null {
+  switch (type) {
+    case 'datetime':
+      const value = cleanText(Cheerio.load(contents).text())
+        .replace(/\u00A0/g, ' ')
+        .trim()
+      const origin = parseDateTime(
+        value,
+        'EEE MMM dd, hh:mm a',
+        new Date()
+      )
+      const time = zonedTimeToUtc(origin, 'America/New_York')
+      return time.toISOString()
+    case 'channel':
+      // const text = cleanText(Cheerio.load(contents).text())
+      // const channelName = 
+      // if (text === '?') {
+      //   return null
+      // }
+      const linkEl = Cheerio.load(contents)('a')
+      if (linkEl.length === 0) {
+        return null
+      }
+      const url = linkEl.attr('href')
+      const name = cleanText(linkEl.text())
+      return { name, url } as TwitchChannel
+    case 'runners':
+      return cleanText(Cheerio.load(contents).text()).split(' vs ')
+    case 'commentary':
+    case 'tracking':
+      return cleanText(Cheerio.load(contents).text())
+        .split(', ')
+        .map((commentator) => commentator.trim())
+    default:
+      return null
+  }
+}
+
+export type Race = {
+  runners?: string[] | null
+  datetime?: string | null
+  channel?: TwitchChannel | null
+  commentary?: string[] | null
+  tracking?: string[] | null
+}
+
+export type TwitchChannel = {
+  name: string
+  url: string
+}
+
+export const fetchCurrentRaces: any = async () => {
+  const races: Race[] = []
+  const HEADINGS = ['datetime', 'runners', 'channel', 'commentary', 'tracking']
+  const url = new URL('https://schedule.speedgaming.org/choozo/')
+  const res = await fetch(url.href)
+  const page = Cheerio.load(await res.text())
+  page('table tbody tr').each((rowIndex, rowEl) => {
+    if (rowIndex !== 0) {
+      const row = page(rowEl)
+      const race = Object.fromEntries(HEADINGS.map((heading) => [heading, null])) as Race
+      row.children('td').each((index, cell: Cheerio.Element) => {
+        const type = HEADINGS[index] as keyof Race
+        const content = parseCellContents(cell, type)
+        // @ts-ignore
+        race[type] = content
+      })
+      races.push(race)
+    }
+  })
+  return races
+}
---
+++ pages/index.tsx
@@ -0,0 +1,85 @@
+import { fetchCurrentRaces, Race } from 'lib/scraper'
+import { format as formatDateTime, utcToZonedTime } from 'date-fns-tz'
+import { Inter } from '@next/font/google'
+
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap'
+})
+
+export type HomeProps = {
+  races: Race[]
+}
+
+const getRaceTime = (time: string, tz: string) => {
+  try {
+    return formatDateTime(utcToZonedTime(time, tz), 'MMM d, h:mm a')
+  } catch (err) {
+    return '-'
+  }
+}
+
+const Dash = () => (
+  <span className="dash">&mdash;</span>
+)
+
+export default function Home({ races = [] }: HomeProps) {
+  const tz = Intl.DateTimeFormat().resolvedOptions().timeZone
+  return (
+    <main className={`container ${inter.className}`}>
+      <h1>Super Metroid Choozo Randomizer<br />2022 Schedule</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Time</th>
+            <th>Players</th>
+            <th>Channel</th>
+            <th>Commentary</th>
+            <th>Tracking</th>
+          </tr>
+        </thead>
+        <tbody>
+          {races.map((race, i) => {
+            // @ts-ignore
+            const time = getRaceTime(race.datetime, tz)
+            return (
+              <tr key={i}>
+                <td>
+                  <span className="mobile-label">Time</span>
+                  {time}
+                </td>
+                <td>
+                  <span className="mobile-label">Players</span>
+                  {race.runners?.join(' vs ')}
+                </td>
+                <td>
+                  <span className="mobile-label">Channel</span>
+                  {race.channel ? (
+                  <a href={race.channel.url}>{race.channel.name}</a>
+                  ) : <Dash />}
+                </td>
+                <td>
+                  <span className="mobile-label">Commentary</span>
+                  {race.commentary?.join(', ') || <Dash />}
+                </td>
+                <td>
+                  <span className="mobile-label">Tracking</span>
+                  {race.tracking?.join(', ') || <Dash />}
+                </td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+    </main>
+  )
+}
+
+export async function getStaticProps() {
+  const races:Promise<Race[]> = await fetchCurrentRaces()
+  return {
+    props: {
+      races,
+    },
+  }
+}
