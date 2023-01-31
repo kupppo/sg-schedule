3e66d5c251ef6feff2ac96eb3eda00565d491b46
kupppo <kupppo@protonmail.com>
2023-01-31 18:04:14 +0000

    discord auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,15 @@ import 'normalize.css'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Analytics } from '@vercel/analytics/react'
+import { SessionProvider } from 'next-auth/react'
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+  Component, pageProps: { session, ...pageProps }
+}: AppProps) {
   return (
-    <>
+    <SessionProvider session={pageProps.session}>
       <Component {...pageProps} />
       <Analytics />
-    </>
+    </SessionProvider>
   )
 }
---
+++ pages/admin/index.tsx
@@ -0,0 +1,27 @@
+import { useSession, signIn, signOut } from 'next-auth/react'
+
+export default function AdminPage() {
+  const { data: session, status } = useSession()
+  return (
+    <div>
+      {status === 'loading' && (
+        <>...</>
+      )}
+      {status === 'authenticated' && (
+        <div>
+          Signed In as: {session.user?.name}<br />
+          <button onClick={() => signOut()}>
+            Sign Out
+          </button>
+        </div>
+      )}
+      {status === 'unauthenticated' && (
+        <div>
+          <button onClick={() => signIn('discord', null, { prompt: 'none' })}>
+            Sign In
+          </button>
+        </div>
+      )}
+    </div>
+  )
+}
