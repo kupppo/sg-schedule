e42e1b87606ba3b316a5aae98ffad2b8ece4e1e9
kupppo <kupppo@protonmail.com>
2023-07-25 12:19:42 +0100

    redirect choozo hostname to /choozo

--- middleware.ts
+++ middleware.ts
@@ -2,14 +2,18 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
  
 export function middleware(request: NextRequest) {
-  const isChoozo = request.nextUrl.hostname === 'choozo-schedule.vercel.app'
+  const { nextUrl } = request
+  const { pathname, hostname } = nextUrl
+  const isChoozo = hostname === 'choozo-schedule.vercel.app'
+  const redirectBase = 'https://sg-schedule.vercel.app'
   if (isChoozo) {
-    const redirectBase = 'https://sg-schedule.vercel.app'
-    return NextResponse.redirect(new URL('/choozo', redirectBase))
+    const redirectPath = (pathname === '/') ? '/choozo' : pathname
+    const redirectURL = new URL(redirectPath, redirectBase)
+    return NextResponse.redirect(redirectURL)
   }
   return NextResponse.next()
 }
  
 export const config = {
-  matcher: '/',
+  matcher: '/*',
 }
