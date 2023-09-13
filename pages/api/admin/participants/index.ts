ff3f0a163334313d6e57db189d8980b2ce5d7488
kupppo <kupppo@protonmail.com>
2023-09-13 14:10:30 +0100

    more admin work

---
+++ pages/api/admin/participants/index.ts
@@ -0,0 +1,15 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import prisma from 'lib/prisma'
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  switch (req.method) {
+    case 'POST':
+      const body = req.body
+      const update = await prisma.participant.create({
+        data: body
+      })
+      return res.json(update)
+    default:
+      return res.status(405).end()
+  }
+}
